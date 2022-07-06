807a0454000d62cd82278449804f5496fb6a1004
양지훈 <kelvin9149@gmail.com>
2022-07-06 11:38:17 +0900

    sample test code

--- api-admin/src/test/java/com/multimodule/msa/AdminApplicationTests.java
+++ api-admin/src/test/java/com/multimodule/msa/AdminApplicationTests.java
@@ -1,13 +1,36 @@
 package com.multimodule.msa;
 
+import com.multimodule.msa.model.User;
+import com.multimodule.msa.repository.UserRepository;
+import com.multimodule.msa.service.UserService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 
 @SpringBootTest
 class AdminApplicationTests {
 
+    @Autowired
+    UserService userService;
+
+    @Autowired
+    UserRepository userRepository;
+
     @Test
-    void contextLoads() {
+    void 사용자_조회() {
+        // given
+        List<User> givenUsers = userRepository.findAll();
+
+        // when
+        List<User> whenUsers = userService.getUser();
+
+        // then
+        assertThat(givenUsers.size()).isEqualTo(whenUsers.size());
     }
 
 }
--- api-main/src/test/java/com/multimodule/msa/ApiApplicationTests.java
+++ api-main/src/test/java/com/multimodule/msa/ApiApplicationTests.java
@@ -1,13 +1,34 @@
 package com.multimodule.msa;
 
+import com.multimodule.msa.model.User;
+import com.multimodule.msa.repository.UserRepository;
+import com.multimodule.msa.service.UserService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class ApiApplicationTests {
 
+    @Autowired
+    UserService userService;
+
+    @Autowired
+    UserRepository userRepository;
+
     @Test
-    void contextLoads() {
+    void 사용자_조회() {
+        // given
+        User user = User.builder().username("이해진").domain("naver.com").nickname("LHJ").build();
+        User givenUser = userRepository.save(user);
+
+        // when
+        User whenUser = userService.getUser("이해진");
+
+        // then
+        assertThat(givenUser.getId()).isEqualTo(whenUser.getId());
     }
 
 }
